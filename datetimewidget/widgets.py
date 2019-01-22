8a5d11ac86b6d741cc5031c8b8af0fbd1b8321ec
Jim Casteleiro <jimcasteleiro@gmail.com>
2019-01-22 14:35:20 -0500

    django 2.1 fix

--- datetimewidget/widgets.py
+++ datetimewidget/widgets.py
@@ -219,9 +219,9 @@ class PickerWidgetMixin(object):
 
         super(PickerWidgetMixin, self).__init__(attrs, format=self.format)
 
-    def render(self, name, value, attrs=None):
+    def render(self, name, value, attrs=None, **kwargs):
         final_attrs = self.build_attrs(attrs)
-        rendered_widget = super(PickerWidgetMixin, self).render(name, value, final_attrs)
+        rendered_widget = super(PickerWidgetMixin, self).render(name, value, final_attrs, **kwargs)
 
         #if not set, autoclose have to be true.
         self.options.setdefault('autoclose', True)
